1bbf29d550d84d68a7842cbe42a4fa20f02912b4
Joel <joel.sanchez78657@gmail.com>
2026-02-02 17:32:42 -0800

    Replaced the ModOptions menu from Dami with the one from Oudstand. Added button to toggle the visual overhaul portion of the mod. Currently not tied to anything. Updated toymaker translation file references to follow naming standard.

--- mods-unpacked/ClassiestKarato-WinterMod/mod_main.gd
+++ mods-unpacked/ClassiestKarato-WinterMod/mod_main.gd
@@ -9,7 +9,7 @@ extends Node
 
 const MOD_DIR = "ClassiestKarato-WinterMod/" # name of the folder that this file is in
 const MYMOD_LOG = "ClassiestKarato-WinterMod" # full ID of your mod (AuthorName-ModName)
-
+const MOD_ID = "WinterMod"
 
 var dir = ""
 var ext_dir = ""
@@ -24,6 +24,8 @@ func _init()->void:
 
 func _ready()->void:
 	ModLoaderLog.info("Ready", MYMOD_LOG)
+	
+	call_deferred("_register_mod_options")
 
 	# ! This uses Godot's native `tr` func, which translates a string. You'll
 	# ! find this particular string in the example CSV here: translations/modname.csv
@@ -31,6 +33,36 @@ func _ready()->void:
 	
 	var ContentLoader = get_node("/root/ModLoader/Darkly77-ContentLoader/ContentLoader")
 	ContentLoader.load_data(dir + "content_data/winter_mod_resources.tres", MYMOD_LOG)
+
+
+func _get_mod_options() -> Node:
+	var parent = get_parent()
+	if not parent:
+		return null
+	var mod_options_mod = parent.get_node_or_null("Oudstand-ModOptions")
+	if not mod_options_mod:
+		return null
+	return mod_options_mod.get_node_or_null("ModOptions")
+
+
+func _register_mod_options() -> void:
+	var mod_options = _get_mod_options()
+	if not mod_options:
+		ModLoaderLog.error("ModOptions not found, cannot register options", MOD_ID)
+		return
+	
+	mod_options.register_mod_options(MOD_ID, {
+		"tab_title": "Winter Mod",
+		"options": [
+			{
+				"type": "toggle",
+				"id": "wintermod_visual_overhaul",
+				"label": "WINTERMOD_VISUAL_OVERHAUL",
+				"default": true
+			}
+		],
+		"info_text": "WINTERMOD_INFO_TEXT"
+	})
 
 
 func add_translations() -> void:
