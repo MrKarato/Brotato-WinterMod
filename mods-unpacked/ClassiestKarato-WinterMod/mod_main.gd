ebb828de308855cd78d316d05b356c27092e8e66
Vex <clarkshayne05@gmail.com>
2026-02-12 22:34:25 -0500

    Dami Mod Options Conversion
    Reimplemented Dami Mod options support, no longer as a dependency but can be used as well.

--- mods-unpacked/ClassiestKarato-WinterMod/mod_main.gd
+++ mods-unpacked/ClassiestKarato-WinterMod/mod_main.gd
@@ -9,12 +9,13 @@ extends Node
 
 const MOD_DIR = "ClassiestKarato-WinterMod/" # name of the folder that this file is in
 const MYMOD_LOG = "ClassiestKarato-WinterMod" # full ID of your mod (AuthorName-ModName)
-const MOD_ID = "WinterMod"
+
 
 var dir = ""
 var ext_dir = ""
 var translations = ""
 
+var mod_options
 
 func _init()->void:
 	dir = ModLoaderMod.get_unpacked_dir().plus_file(MOD_DIR)
@@ -25,44 +26,15 @@ func _init()->void:
 func _ready()->void:
 	ModLoaderLog.info("Ready", MYMOD_LOG)
 	
-	call_deferred("_register_mod_options")
-
+	#Handles the config upon starting
+	init_config()
+	
 	# ! This uses Godot's native `tr` func, which translates a string. You'll
 	# ! find this particular string in the example CSV here: translations/modname.csv
 	ModLoaderLog.info(str("Translation Demo: ", tr("WINTERMOD_READY_TEXT")), MYMOD_LOG)
 	
 	var ContentLoader = get_node("/root/ModLoader/Darkly77-ContentLoader/ContentLoader")
 	ContentLoader.load_data(dir + "content_data/winter_mod_resources.tres", MYMOD_LOG)
-
-
-func _get_mod_options() -> Node:
-	var parent = get_parent()
-	if not parent:
-		return null
-	var mod_options_mod = parent.get_node_or_null("Oudstand-ModOptions")
-	if not mod_options_mod:
-		return null
-	return mod_options_mod.get_node_or_null("ModOptions")
-
-
-func _register_mod_options() -> void:
-	var mod_options = _get_mod_options()
-	if not mod_options:
-		ModLoaderLog.error("ModOptions not found, cannot register options", MOD_ID)
-		return
-	
-	mod_options.register_mod_options(MOD_ID, {
-		"tab_title": "Winter Mod",
-		"options": [
-			{
-				"type": "toggle",
-				"id": "wintermod_visual_overhaul",
-				"label": "WINTERMOD_VISUAL_OVERHAUL",
-				"default": true
-			}
-		],
-		"info_text": "WINTERMOD_INFO_TEXT"
-	})
 
 
 func add_translations() -> void:
@@ -76,3 +48,74 @@ func install_script_extensions() -> void:
 	ModLoaderMod.install_script_extension(ext_dir + "/player.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "/player_run_data.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "/run_data.gd")
+
+#Config funcs 
+
+func init_config() -> void:
+	mod_options = get_node_or_null("/root/ModLoader/dami-ModOptions/ModsConfigInterface")
+	
+	var config := resolve_active_config()
+	ModLoaderConfig.set_current_config(config)
+	
+	if mod_options:
+		var current := ModLoaderConfig.get_current_config(MYMOD_LOG)
+		mod_options.connect("setting_changed", self, "_on_mod_options_setting_changed")
+
+
+func resolve_active_config() -> ModConfig:
+	var current := ModLoaderConfig.get_current_config(MYMOD_LOG)
+
+	# No config exists at all -> start with default
+	if current == null:
+		ModLoaderLog.debug("No existing config detected loading default", MYMOD_LOG)
+		var result_default = ModLoaderConfig.get_default_config(MYMOD_LOG)
+		return result_default
+
+	# If ModOptions is available -> use or create custom config
+	if mod_options:
+		ModLoaderLog.debug("ModOptions enabled ensuring custom config exists", MYMOD_LOG)
+		var result_custom = ensure_custom_config()
+		return result_custom
+	var safe_default = ModLoaderConfig.get_default_config(MYMOD_LOG)
+	return safe_default
+
+
+func ensure_custom_config(config_name: String = "custom",debug_log : bool = true) -> ModConfig:
+	var current_name := ModLoaderConfig.get_current_config_name(MYMOD_LOG)
+
+	# Already custom -> nothing to do
+	if current_name == config_name:
+		if debug_log:
+			ModLoaderLog.debug("Custom config already active (" + current_name + ") -> no action needed", MYMOD_LOG)
+		return ModLoaderConfig.get_current_config(MYMOD_LOG)
+
+	# If custom config exists already -> switch to it
+	var existing = ModLoaderConfig.get_config(MYMOD_LOG, config_name)
+	if existing:
+		if debug_log:
+			ModLoaderLog.debug("Existing custom config found (" + existing.name + ") -> using it.", MYMOD_LOG)
+		return existing
+
+	# Create new custom config
+	if debug_log:
+		ModLoaderLog.debug("Creating new custom config", MYMOD_LOG)
+	var new_config = ModLoaderConfig.create_config(
+		MYMOD_LOG,
+		config_name,
+		ModLoaderConfig.get_default_config(MYMOD_LOG).data
+	)
+	if debug_log:
+		ModLoaderLog.debug("New custom config created -> " + new_config.name, MYMOD_LOG)
+
+	return new_config
+
+
+func _on_mod_options_setting_changed(setting_name, value, mod_name) -> void:
+	if mod_name != MYMOD_LOG:
+		return
+
+	ModLoaderLog.debug("ModOption changed: " + setting_name + " -> " + str(value), MYMOD_LOG)
+	
+	var config := ensure_custom_config("custom",false)
+	config.data[setting_name] = value
+	ModLoaderConfig.update_config(config)
